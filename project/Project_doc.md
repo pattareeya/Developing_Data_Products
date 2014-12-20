3d0d6eaaa071b5642f8df18c53ffcb5635908de0
pattareeya <pattareeya.o@gmail.com>
2014-12-20 15:29:36 -0600

    upload project

---
+++ project/Project_doc.md
@@ -0,0 +1,9 @@
+### MPG Prediction
+
+We are interested in exploring the relationship between a set of variables and miles per gallon (MPG) (outcome). 
+
+We want to find the best fit model to predict MPG by using the dataset *mtcars* which was extracted from the 1974 Motor Trend US magazine and comprises fuel consumption and 10 aspects of automobile design and performance for 32 automobiles (1973-74 models)
+
+In this application, we found that only cyliders, horse powers, weights and transmission types played the important role on the prediction.
+
+Please select the values on the left panel and then the result of prediction will be on the right hand side.
---
+++ project/ui.R
@@ -0,0 +1,45 @@
+library(shiny)
+shinyUI(pageWithSidebar(
+    headerPanel("Prediction MPG using regression model on mtcars data set"),
+        sidebarPanel(
+            #Generate the inputs for selection
+            h2('Please select'),
+            radioButtons("cyl", "Cylinder:",
+                         c("4" = "4",
+                           "6" = "6",
+                           "8" = "8")),
+            selectInput("am", "Transmission:",
+                        c("Automatic" = "Automatic",
+                          "Manual" = "Manual")),
+            sliderInput('hp', 'Horse power', 52, min = 52, max = 335, step = 1),
+            sliderInput('wt', 'Weight (lb/1000)', 1, min = 1, max = 5, step = 0.15),
+            submitButton('Submit')
+            ),
+        mainPanel(
+            tabsetPanel(
+                tabPanel('Prediction',
+                #Showing the results and prediction value, also plots
+                    h2('Results:'),
+                    h3('You have entered:'),
+                    h4('Cylider:'),
+                    verbatimTextOutput("inputcyl"),
+                    h4('Transmission:'),
+                    verbatimTextOutput("inputam"),
+                    h4('Horse Power:'),
+                    verbatimTextOutput("inputhp"),
+                    h4('Weight (lb/1000):'),
+                    verbatimTextOutput("inputwt"),
+                    h4('Prediction Results'),
+                    verbatimTextOutput("predictmpg")
+                    
+                ),
+                tabPanel('Plot',
+                    plotOutput('plotxy')
+                ),
+                tabPanel('Documentation',
+                   includeMarkdown("Project_doc.md")      
+                )
+            )
+        )
+    )
+)
